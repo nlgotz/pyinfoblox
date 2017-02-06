dc9d1d309c45758c36e87a901a276010e0807c19
Nathan Gotz <nlgotz+github@gotzweb.com>
2017-02-05 18:28:53 -0600

    added get_next_available_network

--- ibclient/ibclient.py
+++ ibclient/ibclient.py
@@ -162,6 +162,7 @@ class IBClient(object):
         :param cidr: New network's cidr Address
         """
         container = self._get("networkcontainer?network=" + network)
+        print container
         ref = container[0]['_ref']
         find = ref.find(":")
         ref = ref[0:find]
