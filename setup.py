1f454cf94565c058e9ea9fbee52e13729c21401d
Nathan Gotz <nlgotz+github@gotzweb.com>
2018-03-04 21:30:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ibclient',
-    version='0.0.7.1',
+    version='0.0.7.2',
     description="Access Infoblox API",
     classifiers=[],
     keywords='infoblox api infobloxapi',
